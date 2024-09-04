7457c5f72ccd4473d6eed24334a4e84f6b460463
JimmyCai-16 <jcai6116@uni.sydney.edu.au>
2024-09-04 13:54:26 +1000

    Add Initially Form Validation

--- web/porphyry_deposits/urls.py
+++ web/porphyry_deposits/urls.py
@@ -9,6 +9,9 @@ app_name = "porphyry_deposits"
 urlpatterns = [
     path('get_deposits/', views.get_deposits, name='get_deposits'),
     path('prediction/', views.prediction, name='prediction'),
+    path('validate_latitude/', views.validate_latitude, name='validate_latitude'),
+    path('validate_longitude/', views.validate_longitude, name='validate_longitude'),
+    path('prediction_results/', views.prediction_results, name='prediction_results'),
 
  
 ]
--- web/porphyry_deposits/views.py
+++ web/porphyry_deposits/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from typing import Dict
 
 
 # Create your views here.
@@ -25,14 +27,81 @@ def prediction(request):
     longitude = request.POST.get('longitude')
     latitude = request.POST.get('latitude')
 
-    print(f"Longitude: {longitude}, Latitude: {latitude}")
-
     # Send the parameters to the model
     params = {
         'longitude': longitude,
         'latitude': latitude
     }
+    
+    return render(request, 'prediction_result.html', params)
 
+# Validate the user input form
+# Dual authentication to prevent users from overstepping front-end authentication
+@login_required
+@require_POST
+def validate_latitude(request):
+    latitude = request.POST.get('latitude')
+
+    errors = {}
+    if latitude and not is_valid_latitude(latitude):
+        errors['latitude'] = 'Invalid latitude value.'
+
+    context = {
+        'errors': errors
+    }
+
+    print("errors: ", errors)
+
+    return render(request, 'utils/error_message.html', context)
+
+# Dual authentication to prevent users from overstepping front-end authentication
+@login_required
+@require_POST
+def validate_longitude(request):
+    longitude = request.POST.get('longitude')
+    latitude = request.POST.get('latitude')
+
+    errors = {}
+    if longitude and not is_valid_longitude(longitude):
+        # Note that the names should correspond
+        errors['longitude'] = 'Invalid longitude value.'
+
+    context = {
+        'errors': errors
+    }
+
+    print("errors: ", errors)
+
+    return render(request, 'utils/error_message.html', context)
+
+@login_required
+def prediction_results(request):
+    
+    longitude = request.POST.get('longitude')
+    latitude = request.POST.get('latitude')
+
+    context = {
+        'longitude': longitude,
+        'latitude': latitude,
+    }
+    
     # TODO: Write a new method to handle the AI-model, and then send the result to 'prediction_result.html' 
 
-    return render(request, 'prediction_result.html', params)
+    
+    return render(request, 'prediction_result.html', context)
+
+
+def is_valid_longitude(value):
+    try:
+        lon = float(value)
+        return -180 <= lon <= 180
+    except ValueError:
+        return False
+
+def is_valid_latitude(value):
+    try:
+        lat = float(value)
+        return -90 <= lat <= 90
+    except ValueError:
+        return False
+
